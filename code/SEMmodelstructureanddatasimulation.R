cb41129b3fb6dc7c185affee1eef9c0115be0e9a
Silke Bachhuber <smbachhuber@gmail.com>
2019-05-22 17:18:03 -0700

    updating indirect effects code

---
+++ code/SEMmodelstructureanddatasimulation.R
@@ -0,0 +1,15 @@
+library(lavaan)
+help("simulateData")
+
+#specify model
+SP.model <- '
+            #measurement model
+            lepta =~ x1 + x2 + x3 + x4
+            nucella =~ x1 + x2 + x3 + x4
+            climate =~ y1 + y2
+            #regressions
+            lepta ~ climate
+            nucella ~ climate
+            lepta ~ nucella
+            #residual (co)variances
+            y1 ~~ y2'
